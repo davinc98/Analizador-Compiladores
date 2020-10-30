d0e342b1b903f81e6d6bdbe6d0ad1a59fd3dc4c4
Senko <dankuso9919@gmail.com>
2020-10-29 18:12:03 -0600

    Main actualizado

--- src/analizadorcompiladores/Principal.java
+++ src/analizadorcompiladores/Principal.java
@@ -20,49 +20,53 @@ public class Principal {
         
         int opcionMenu;
         Scanner leer = new Scanner(System.in);
+        boolean salir=false;
         
-        System.out.println("-------MENU-------");
-        System.out.println("1. Crear AFN básico."); //In: un caracter, y ponerle ID (para cerradura, etc)
-        System.out.println("2. Unir 2 AFN."); //In: 2do AFN, ID para AFN unión, Out: AFN unión
-        System.out.println("3. Concatenar 2 AFN.");
-        System.out.println("4. Cerradura Transitiva o de Kleene");
-        System.out.println("5. Unión para Analizador Léxico."); //qué automata quiero unir (mostrar tabla con automatas)
-        System.out.println("6. Convertir AFN a AFD."); //cualquier AFN, hasta el de la opcion 6. In: ID del AFN
-        System.out.println("7. Validar cadena usando un AFN o un AFD."); //seleccionar AFN o AFD de los ya ingresados
-        System.out.println("8. Opcional.");
-        System.out.println("9. Salir");
-        System.out.print("Selecciona el número de la opción deseada: ");
-        opcionMenu = leer.nextInt();
-        
-        switch(opcionMenu){
-            case 1: //crear AFN
-                System.out.print("Ingresa el caracter para la transición del AFN: ");
-                char c = leer.next().charAt(0);
-                System.out.println("C: "+c);
-                miAFN = new AFN();
-                miAFN.crearBasico(c);
-                listaAFN.add(miAFN);
-                break;
-            case 2: //Unir AFN
-                break;
-            case 3: //concatenar AFN
-                System.out.println("hola");
-                break;
-            case 4: //Cerradura
-                break;
-            case 5: //Analizador Léxico
-                System.out.println("hola");
-                break;
-            case 6: //Convertir AFN a AFD
-                break;
-            case 7: //Validar cadena
-                System.out.println("hola");
-                break;
-            case 8: //Opcional
-                break;
-            default:
-                System.out.println("Hasta luego!");
-                break;
+        while(salir==false){
+            System.out.println("-------MENU-------");
+            System.out.println("1. Crear AFN básico."); //In: un caracter, y ponerle ID (para cerradura, etc)
+            System.out.println("2. Unir 2 AFN."); //In: 2do AFN, ID para AFN unión, Out: AFN unión
+            System.out.println("3. Concatenar 2 AFN.");
+            System.out.println("4. Cerradura Transitiva o de Kleene");
+            System.out.println("5. Unión para Analizador Léxico."); //qué automata quiero unir (mostrar tabla con automatas)
+            System.out.println("6. Convertir AFN a AFD."); //cualquier AFN, hasta el de la opcion 6. In: ID del AFN
+            System.out.println("7. Validar cadena usando un AFN o un AFD."); //seleccionar AFN o AFD de los ya ingresados
+            System.out.println("8. Opcional.");
+            System.out.println("9. Salir");
+            System.out.print("Selecciona el número de la opción deseada: ");
+            opcionMenu = leer.nextInt();
+
+            switch(opcionMenu){
+                case 1: //crear AFN
+                    System.out.print("Ingresa el caracter para la transición del AFN: ");
+                    char c = leer.next().charAt(0);
+                    System.out.println("C: "+c);
+                    miAFN = new AFN();
+                    miAFN.crearBasico(c);
+                    listaAFN.add(miAFN);
+                    break;
+                case 2: //Unir AFN
+                    break;
+                case 3: //concatenar AFN
+                    System.out.println("hola");
+                    break;
+                case 4: //Cerradura
+                    break;
+                case 5: //Analizador Léxico
+                    System.out.println("hola");
+                    break;
+                case 6: //Convertir AFN a AFD
+                    break;
+                case 7: //Validar cadena
+                    System.out.println("hola");
+                    break;
+                case 8: //Opcional
+                    break;
+                default:
+                    salir=true;
+                    System.out.println("Hasta luego!");
+                    break;
+            }
         }
     }
 
